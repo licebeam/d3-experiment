9ee4688c7efff123f441524d6c122254a36df13a
licebeam <dustinwalkerart@gmail.com>
2018-11-01 17:28:32 +0900

    added piechart demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3'; //import all of d3
 
 const Container = styled.div`
   background-color: white;
-  height: 100vh;
+  height: 100%;
   background: linear-gradient(270deg, #dc64ff, #ffd364);
   background-size: 400% 400%;
   animation: AnimationName 30s ease infinite;
@@ -62,7 +62,11 @@ const Container = styled.div`
       }
     }
     }
-    
+    .pie-chart{
+      display: flex;
+      justify-content: center;
+      padding: 10px;
+    }
 `
 class App extends Component {
   constructor(props) {
@@ -72,10 +76,53 @@ class App extends Component {
         1, 2, 3, 10, 20, 100, 1000, 25, 800,
       ],
       screenHeight: 100,
-      currentData: null
+      currentData: null,
+      
     }
   }
- 
+  createPieChart = () => {
+    var data = [10, 20, 100];
+
+    var width = 160,
+      height = 160,
+      radius = Math.min(width, height) / 2;
+
+    var color = d3.scaleOrdinal()
+      .range(["#98abc5", "#8a89a6", "#7b6888"]);
+
+    var arc = d3.arc()
+      .outerRadius(radius - 10)
+      .innerRadius(0);
+
+    var labelArc = d3.arc()
+      .outerRadius(radius - 40)
+      .innerRadius(radius - 40);
+
+    var pie = d3.pie()
+      .sort(null)
+      .value(function (d) { return d; });
+
+    var svg = d3.select(".pie-chart").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var g = svg.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+    g.append("path")
+      .attr("d", arc)
+      .style("fill", function (d) { return color(d.data); });
+
+    g.append("text")
+      .attr("transform", function (d) { return "translate(" + labelArc.centroid(d) + ")"; })
+      .attr("dy", ".35em")
+      .text(function (d) { return d.data; });
+  }
+
   createBarChart = () => {
     var that = this // rebinds this to call state
     d3.select('.App').selectAll("style-bar").remove(); //removes old data and allows us to add new data
@@ -123,6 +170,7 @@ class App extends Component {
   }
   componentDidMount() {
     this.createBarChart();
+    this.createPieChart();
   }
 
   componentDidUpdate(prevProps, prevState) {//REMEMBER TO USE BOTH ARGUMENTS
@@ -143,7 +191,8 @@ class App extends Component {
         
         <div className="App" />
         
-        <div className ='data'>{this.state.currentData}</div>
+        <div className='data'>{this.state.currentData}</div>
+        <div className='pie-chart'/>
       </Container>
     );
   }
