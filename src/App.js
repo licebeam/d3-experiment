7505456178b984639bbd6208077c952b26baab24
licebeam <dustinwalkerart@gmail.com>
2018-11-01 14:01:53 +0900

    added scaling to flip the barChart

--- src/App.js
+++ src/App.js
@@ -5,9 +5,17 @@ import * as d3 from 'd3'; //import all of d3
 const Container = styled.div`
   background-color: black;
   .App{
+    display: flex;
     color: white;
-    div{
+    font-size: .8rem;
+    transform: scaleY(-1);
+    style-bar{
       background-color: red;
+      flex: 1;
+      padding: 10px;
+      border: 1px solid white;
+      margin: 4px;
+      transform: scaleY(-1);
     }
   }
 `
@@ -15,19 +23,19 @@ const Container = styled.div`
 class App extends Component {
   state = {
     fakeDataArr: [
-      1, 2, 3, 10, 20, 100,
+      1, 2, 3, 10, 20, 100, 1000, 25, 800,
     ]
   }
   createBarChart = () => {
-    let x = d3.scaleLinear()
+    let bar = d3.scaleLinear()
       .domain([0, d3.max(this.state.fakeDataArr)])
-      .range([0, 420]);
+      .range([0, 400]); //this is the height of the chart.
     d3.select('.App')
       .selectAll('div')
       .data(this.state.fakeDataArr)
-      .enter().append('div')
-      .style('width', function (d) { return x(d) + 'px'; })
-      .text(function (d) { return d; });
+      .enter().append('style-bar')
+      .style('height', (data) =>  { return bar(data) + 'px'; }) //the length of the bar chart.
+      .text((data) => { return data; });
   }
   componentDidMount() {
     this.createBarChart();
